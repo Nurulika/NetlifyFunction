74ee5e15f1672021aa65482559cef34833641043
Nuru <ceruks@eteb.me>
2025-10-03 23:07:26 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/netlifyfunction/main.go
@@ -0,0 +1,23 @@
+// cmd/netlifyfunction/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "netlifyfunction/internal/netlifyfunction"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := netlifyfunction.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
